79d49a5fb0753fbc19d6ac2e50c59318ad122b68
BhaskarRay <bhaskar111078.cse@gmail.com>
2018-11-26 09:08:20 -0400

    helioviewer images download and draw polygon commit

--- test/QueryHek.py
+++ test/QueryHek.py
@@ -1,7 +1,5 @@
-# A QueryHek takes a path to input configuration file (including file name) and path to output folder
-# and generates a XML or JSON file by querying HEK API. A query is constructed from the given search conditions in input configuration file
 # @author Bhaskar Ray
-# @version 11/18/2018
+# @version 11/26/2018
 
 import urllib.request
 import urllib.parse
@@ -10,13 +8,12 @@ import json as j
 import pandas as pd
 import datetime
 from pgmagick import Image
-import PIL as p
 import cv2
+import os
+import numpy as np
 
 
-
-def main():
-
+def hek():
     startYear = ''
     startMonth = ''
     startDay = ''
@@ -40,7 +37,7 @@ def main():
     filterOperators = []
     valuesLoop = []
     fileName = ''
-    opDirectory=''
+    opDirectory = ''
     json = False
     xml = False
     filter = False
@@ -48,13 +45,6 @@ def main():
     EVENT_REGION = 'all'
     RESULT_LIMIT = '200'
     ENCODING_SCHEME = 'utf-8'
-
-
-
-
-
-
-
 
     try:
 
@@ -64,10 +54,10 @@ def main():
 
         if "JSON" in dataType:
             url = 'https://www.lmsal.com/hek/her?cosec=2&&cmd=search&type=column'
-            json=True
+            json = True
         elif "XML" in dataType:
             url = 'https://www.lmsal.com/hek/her?cosec=1&&cmd=search&type=column'
-            xml=True
+            xml = True
 
         with open(inputFileName, 'r') as f:
             contents = f.readlines()
@@ -113,7 +103,7 @@ def main():
 
             elif 'Event Type' in tokens[0]:
 
-                eventModule =[tokens[1].split(',')]
+                eventModule = [tokens[1].split(',')]
                 eventModuleString = ",".join(eventModule[0]).strip()
 
 
@@ -122,18 +112,18 @@ def main():
                 for tokenValue in tokens:
                     if ',' in tokenValue:
                         event = tokenValue.split(',')
-                        coordinateValue=event[0].strip()
-                        x1Value=event[1].strip()
-                        x2Value=event[2].strip()
-                        y1Value=event[3].strip()
-                        y2Value=event[4].strip()
+                        coordinateValue = event[0].strip()
+                        x1Value = event[1].strip()
+                        x2Value = event[2].strip()
+                        y1Value = event[3].strip()
+                        y2Value = event[4].strip()
 
 
             else:
                 if tokens[0]:
                     filterCondition.append(tokens[0].strip())
                     opVal = tokens[1].split(',')
-                    filterOperators.append( opVal[0].strip())
+                    filterOperators.append(opVal[0].strip())
                     filterConditionValue.append(opVal[1].strip())
                     filter = True
                 else:
@@ -147,14 +137,13 @@ def main():
     except Exception as e:
         print("Input Error: ", str(e))
 
-
     try:
 
         values = {'event_type': eventModuleString, 'event_region': EVENT_REGION, 'event_coordsys': coordinateValue,
                   'x1': x1Value, 'x2': x2Value,
                   'y1': y1Value, 'y2': y2Value, 'result_limit': RESULT_LIMIT,
-                  'event_starttime': startYear +"-"+ startMonth +"-"+ startDay +"T"+ startHour +":"+startMinute+":"+ startSecond,
-                  'event_endtime': endYear +"-"+ endMonth+"-"+ endDay +"T"+ endHour +":"+endMinute+":"+ endSecond}
+                  'event_starttime': startYear + "-" + startMonth + "-" + startDay + "T" + startHour + ":" + startMinute + ":" + startSecond,
+                  'event_endtime': endYear + "-" + endMonth + "-" + endDay + "T" + endHour + ":" + endMinute + ":" + endSecond}
 
         values1 = values.copy()
         print(values1)
@@ -168,20 +157,18 @@ def main():
                 values1.update(valuesLoop[i])
             print(values1)
 
-
         data = urllib.parse.urlencode(values1)
         data = data.encode(ENCODING_SCHEME)
-        req = urllib.request.Request(url,data)
+        req = urllib.request.Request(url, data)
         resp = urllib.request.urlopen(req)
         resData = resp.read().decode('utf-8')
 
-
         if json:
-            fileName = "'"+eventModuleString+"'"+"_event_startdate=" + startYear.strip() + "-" + startMonth.strip() + "-" + startDay.strip() + "T" +  startHour +"-"+startMinute+"-"+ startSecond + "_event_enddate=" + endYear.strip() + "-" + endMonth.strip() + "-" + endDay.strip() + "T" +endHour +"-"+endMinute+"-"+ endSecond+ ".json"
+            fileName = "'" + eventModuleString + "'" + "_event_startdate=" + startYear.strip() + "-" + startMonth.strip() + "-" + startDay.strip() + "T" + startHour + "-" + startMinute + "-" + startSecond + "_event_enddate=" + endYear.strip() + "-" + endMonth.strip() + "-" + endDay.strip() + "T" + endHour + "-" + endMinute + "-" + endSecond + ".json"
         elif xml:
-            fileName = "'"+eventModuleString +"'"+"_event_startdate=" + startYear.strip() + "-" + startMonth.strip() + "-" + startDay.strip() + "T" + startHour + "-" + startMinute + "-" + startSecond + "_event_enddate=" + endYear.strip() + "-" + endMonth.strip() + "-" + endDay.strip() + "T" + endHour + "-" + endMinute + "-" + endSecond + ".xml"
+            fileName = "'" + eventModuleString + "'" + "_event_startdate=" + startYear.strip() + "-" + startMonth.strip() + "-" + startDay.strip() + "T" + startHour + "-" + startMinute + "-" + startSecond + "_event_enddate=" + endYear.strip() + "-" + endMonth.strip() + "-" + endDay.strip() + "T" + endHour + "-" + endMinute + "-" + endSecond + ".xml"
 
-        saveFile= open(opDirectory+fileName, 'w')
+        saveFile = open(opDirectory + fileName, 'w')
         saveFile.write(str(resData))
         saveFile.close()
 
@@ -189,10 +176,10 @@ def main():
         print('Error:', str(e))
 
     inputCsvFilePath = input("Enter output directory path for csv file: ")
-    with open(opDirectory+fileName, 'r') as f:
+    with open(opDirectory + fileName, 'r') as f:
         result_parsedData = j.load(f)
         resultParsed = result_parsedData['result']
-    result_data = open(inputCsvFilePath + fileName+ "_result.csv", 'w')
+    result_data = open(inputCsvFilePath + fileName + "_result.csv", 'w')
     csvwriter = csv.writer(result_data)
     count = 0
     for rslt in resultParsed:
@@ -211,12 +198,11 @@ def main():
         f.close()
         contentsCSV = [contentData.strip('\n') for contentData in contentsCSV]
 
-    df = pd.read_csv( inputCsvFilePath + fileName+ "_result.csv", usecols= contentsCSV)
-    df.to_csv(inputCsvFilePath + fileName+ "_Extractedresult.csv", encoding='utf-8', index=False)
+    df = pd.read_csv(inputCsvFilePath + fileName + "_result.csv", usecols=contentsCSV)
+    df.to_csv(inputCsvFilePath + fileName + "_Extractedresult.csv", encoding='utf-8', index=False)
 
 
-
-
+def helioviewer():
     # try:
     #
     #     inputCsvDataFile = input("Enter input csv data file directory path with csv data file name: ")
@@ -260,60 +246,82 @@ def main():
     # except Exception as e:
     #     print('Error:', str(e))
 
-
-
-
-# Jp2 Jpeg format solar image downloader---------------------------------------------------------------------------------------------------------------------------------------
-
-
-
+    # Jp2 Jpeg format solar image downloader--------------------------------------------------------------------------------------------------------------------------------------
 
     try:
 
-        #a = datetime.datetime(2012,7,7,23,34,8)
-
-
-        # a=datetime.datetime.strptime("2014-02-08T04:05:55","%Y-%m-%dT%H:%M:%S")
-        # b = datetime.datetime.strptime("2014-02-08T08:05:55", "%Y-%m-%dT%H:%M:%S")
-        # while a<b:
-        #     a=a+ datetime.timedelta(seconds=7200)
-        #     print(a)
-
-        # event_startTimeData = "2014-02-08T04:05:55"
-        # event_endTimeDate = "2014-02-08T08:05:55"
-        # start = datetime.datetime.strptime(event_startTimeData, "%Y-%m-%dT%H:%M:%S")
-        # end = datetime.datetime.strptime(event_endTimeDate, "%Y-%m-%dT%H:%M:%S")
-        # while start <= end:
-        #     event_startTimeData = str(start.date()) + "T" + str(start.time())
-        #     print(event_startTimeData)
-        #     start = start + datetime.timedelta(seconds=7200)
-        #     print(start)
-
-
-
+        CDELT = float(0.599733)
+        HPCCENTER = float(4096.0 / 2.0)
+        # hpc_bboxPolygon = "POLYGON((-471.9 536.7,-315.3 536.7,-315.3 685.5,-471.9 685.5,-471.9 536.7))"
+        # Coordinates= hpc_bboxPolygon.replace('POLYGON((', '?').replace(" ",'?').replace(",", '?').replace("))",'?').split('?')
+        # x1=round(float(HPCCENTER+(float(Coordinates[1])/CDELT)))
+        # y1=round(float(HPCCENTER-(float(Coordinates[2])/CDELT)))
+        # x2=round(float(HPCCENTER+(float(Coordinates[3])/CDELT)))
+        # y2=round(float(HPCCENTER-(float(Coordinates[4])/CDELT)))
+        # x3 = round(float(HPCCENTER+(float(Coordinates[5])/CDELT)))
+        # y3 = round(float(HPCCENTER-(float(Coordinates[6])/CDELT)))
+        # x4 = round(float(HPCCENTER+(float(Coordinates[7])/CDELT)))
+        # y4 = round(float(HPCCENTER-(float(Coordinates[8])/CDELT)))
+        # x5 = round(float(HPCCENTER+(float(Coordinates[9])/CDELT)))
+        # y5 = round(float(HPCCENTER-(float(Coordinates[10])/CDELT)))
+        # print(x1)
+        # print(y1)
+        # print(x2)
+        # print(y2)
+        # print(x3)
+        # print(y3)
+        # print(x4)
+        # print(y4)
+        # print(x5)
+        # print(y5)
         inputCsvDataFile = input("Enter input csv data file directory path with csv data file name: ")
-        data = pd.read_csv(inputCsvDataFile, usecols=['event_starttime', 'event_endtime'])
-        event_startTime = data.event_starttime.tolist()
-        event_endTime = data.event_endtime.tolist()
+        dataNew = pd.read_csv(inputCsvDataFile, usecols=['track_id', 'event_starttime', 'event_endtime', 'hpc_bbox'])
+        track_id = dataNew.track_id.tolist()
+        event_startTime = dataNew.event_starttime.tolist()
+        event_endTime = dataNew.event_endtime.tolist()
+        hpc_bboxPolygon = dataNew.hpc_bbox.tolist()
         directoryJp2 = input("Enter Jp2 image output directory path: ")
         directoryJpg = input("Enter Jpeg image output directory path: ")
-        # directoryJp2 = "G:\imageJP2"
-        # directoryJpg = "G:\imageJPEG"
-        # print(event_startTime[0])
-        # print(event_endTime[0])
-        counter=0
+        directoryJpgPoly = input("Enter Jpeg image with polygon output directory path: ")
+        counter = 0
         for event_startTimeData in event_startTime:
-            # event_startTimeData = "2014-02-08T04:05:55"
-            # event_endTimeDate = "2014-02-08T08:05:55"
-            event_endTimeDate=event_endTime[counter]
-            counter=counter+1
+            track_idData = track_id[counter]
+            event_endTimeDate = event_endTime[counter]
+            hpc_bboxPolygonData = hpc_bboxPolygon[counter]
+            Coordinates = hpc_bboxPolygonData.replace('POLYGON((', '?').replace(" ", '?').replace(",", '?').replace(
+                "))", '?').split('?')
+            x1 = round(float(HPCCENTER + (float(Coordinates[1]) / CDELT)))
+            y1 = round(float(HPCCENTER - (float(Coordinates[2]) / CDELT)))
+            x2 = round(float(HPCCENTER + (float(Coordinates[3]) / CDELT)))
+            y2 = round(float(HPCCENTER - (float(Coordinates[4]) / CDELT)))
+            x3 = round(float(HPCCENTER + (float(Coordinates[5]) / CDELT)))
+            y3 = round(float(HPCCENTER - (float(Coordinates[6]) / CDELT)))
+            x4 = round(float(HPCCENTER + (float(Coordinates[7]) / CDELT)))
+            y4 = round(float(HPCCENTER - (float(Coordinates[8]) / CDELT)))
+            event_startTimeDataFolder = event_startTimeData
+            event_endTimeDateFolder = event_endTimeDate
+            directoryJp2TrackID = directoryJp2 + "\\" + str(track_idData) + "_" + event_startTimeDataFolder.replace(":",
+                                                                                                                    "_") + "_" + event_endTimeDateFolder.replace(
+                ":", "_") + "_Count" + str(counter)
+            print(directoryJp2TrackID)
+            if not os.path.isdir(directoryJp2TrackID):
+                os.makedirs(directoryJp2TrackID)
+            directoryJpgTrackID = directoryJpg + "\\" + str(track_idData) + "_" + event_startTimeDataFolder.replace(":",
+                                                                                                                    "_") + "_" + event_endTimeDateFolder.replace(
+                ":", "_") + "_Count" + str(counter)
+            if not os.path.isdir(directoryJpgTrackID):
+                os.makedirs(directoryJpgTrackID)
+            directoryJpgPolyTrackID = directoryJpgPoly + "\\" + str(
+                track_idData) + "_" + event_startTimeDataFolder.replace(":",
+                                                                        "_") + "_" + event_endTimeDateFolder.replace(
+                ":", "_") + "_Count" + str(counter)
+            if not os.path.isdir(directoryJpgPolyTrackID):
+                os.makedirs(directoryJpgPolyTrackID)
+            counter = counter + 1
             start = datetime.datetime.strptime(event_startTimeData, "%Y-%m-%dT%H:%M:%S")
             end = datetime.datetime.strptime(event_endTimeDate, "%Y-%m-%dT%H:%M:%S")
-            # directoryJp2 = "F:\QueryHekPy\output\images"
-            # directoryJpg = "F:\QueryHekPy\output\imagesJPG"
-
             while start <= end:
-                dateDownload = str(start.date()) + "T" + str(start.time())+"Z"
+                dateDownload = str(start.date()) + "T" + str(start.time()) + "Z"
                 print(dateDownload)
                 uriHelioviewer = "https://api.helioviewer.org/v2/getJP2Image/?"
                 valuesImageUri = {"date": dateDownload, "sourceId": "11",
@@ -329,14 +337,17 @@ def main():
                 urlHelioviewer = "https://api.helioviewer.org/v2/getJP2Image/?"
                 valuesImage = {"date": dateDownload, "sourceId": "11"}
                 dataImage = urllib.parse.urlencode(valuesImage)
-                #directory = "F:\QueryHekPy\output\images"
-                #directory = "F:\QueryHekPy\output\imagesJPG"
-                urllib.request.urlretrieve(urlHelioviewer + dataImage, directoryJp2 + resImageUriData + ".jp2")
-                img = Image(directoryJp2 + resImageUriData + ".jp2")  # Input Image
-                img.write(directoryJpg + resImageUriData + ".jpeg")  # Output Image
-                # im2=cv2.imread(directory + resImageUriData + ".jpeg")
-                # cv2.rectangle(im2, (714, 2474), (809, 2528), (0, 0, 255), 5)
-                # cv2.imwrite("F:\QueryHekPy\output\imagesPoly"+resImageUriData + ".jpeg",im2)
+                urllib.request.urlretrieve(urlHelioviewer + dataImage, directoryJp2TrackID + resImageUriData + ".jp2")
+                img = Image(directoryJp2TrackID + resImageUriData + ".jp2")
+                img.write(directoryJpgTrackID + resImageUriData + ".jpeg")
+                im2 = cv2.imread(directoryJpgTrackID + resImageUriData + ".jpeg")
+                pts = np.array([[x1, y1], [x2, y2], [x3, y3], [x4, y4]], np.int32)
+                pts = pts.reshape((-1, 1, 2))
+                cv2.polylines(im2, [pts], True, (0, 0, 255), 5)
+                cv2.imwrite(directoryJpgPolyTrackID + resImageUriData + ".jpeg", im2)
+                # im2=cv2.imread(directoryJpgTrackID + resImageUriData + ".jpeg")
+                # cv2.rectangle(im2, (714, 2474), (809, 2528), (0, 0, 0), 5)
+                # cv2.imwrite(directoryJpgPoly+resImageUriData + ".jpeg",im2)
                 start = start + datetime.timedelta(seconds=36)
                 print(start)
 
@@ -345,20 +356,14 @@ def main():
         print('Error:', str(e))
 
 
+def main():
+    functionality = input("Choose functionality(hek/helioviewer): ")
+
+    if "hek" in functionality:
+        hek()
+    elif "helioviewer" in functionality:
+        helioviewer()
+
+
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
